fcc7ceca3fc694e18ad820f1c6567e6da0a84a49
Bruno <b_beraldi@hotmail.com>
2025-03-18 20:55:44 -0300

    Hit box Collisions HUD Score

--- code/Enemy.py
+++ code/Enemy.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from Entity import Entity
+
 from code.Const import ENTITY_SPEED, WIN_WIDHT, ENTITY_SHOT_DELAY
 from code.EnemyShot import EnemyShot
+from code.Entity import Entity
 
 
 class Enemy(Entity):
@@ -14,10 +15,8 @@ class Enemy(Entity):
     def move(self, ):
         self.rect.centerx -= ENTITY_SPEED[self.name]
 
-
     def shoot(self):
         self.shot_delay -= 1
         if self.shot_delay == 0:
             self.shot_delay = ENTITY_SHOT_DELAY[self.name]
             return EnemyShot(name=f'{self.name}Shot', position=(self.rect.centerx, self.rect.centery))
-
